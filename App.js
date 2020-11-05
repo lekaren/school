5eaa7246383d914b5a167f8fba59b330a9588dd0
Dabin <cpdnql@naver.com>
2020-11-05 16:52:08 +0900

    then -> async

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import { WebView } from 'react-native-webview';
 import { Notifications } from 'expo';
 import * as Permissions from 'expo-permissions';
 import axios from 'axios';
+import Axios from 'axios';
 
-const PUSH_REGISTRATION_ENDPOINT = 'http://075bdb947218.ngrok.io/token';
-const MESSAGE_ENPOINT = 'http://075bdb947218.ngrok.io/message';
+const PUSH_REGISTRATION_ENDPOINT = 'http://13.125.246.18:3000/token';
+const MESSAGE_ENPOINT = 'http://13.125.246.18:3000/message';
 
 export default function App() {
 
@@ -53,10 +54,15 @@ export default function App() {
     registerForPushNotificationsAsync();
   }, []);
 
+  const getlunch = async() => {
+    const data = await Axios.get("http://13.125.246.18:3000/lunch");
+    const res = data.data.match(/[^<p>.*?</p>]/g).join("").match(/[^</1>]/g).join("").match(/[^a-zA-Z!="'오늘의 급식메뉴]/g).join("").trim();
+    sendMessage(res)
+  }
   useEffect(() => {
-    axios.get("http://13.125.246.18:3000/launch").then(res => res.data.match(/[^<p>.*?</p>]/g).join("").match(/[^</1>]/g).join("").match(/[^a-zA-Z!="'오늘의 급식메뉴]/g).join("").trim())
-    .then(res => sendMessage(res))
+    getlunch()
   },[])
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
